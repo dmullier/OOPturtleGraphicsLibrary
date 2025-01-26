3b993c8e4b8cd39626d214d4443c3dc262142e63
Duncan Mullier <D.Mullier@leedsbeckett.ac.uk>
2025-01-26 17:16:04 +0000

    Updated jar and docs.

--- docs/type-search-index.js
+++ docs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"uk.ac.leedsbeckett.oop","l":"CommandLineInterface"},{"p":"uk.ac.leedsbeckett.oop","l":"LBU_GUI"},{"p":"uk.ac.leedsbeckett.oop","l":"LBUGraphics"},{"p":"uk.ac.leedsbeckett.oop","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"uk.ac.leedsbeckett.oop","l":"CommandLineInterface"},{"p":"uk.ac.leedsbeckett.oop","l":"LBUGraphics"}];updateSearchResults();
